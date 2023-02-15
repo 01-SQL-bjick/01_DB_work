f1847954727d3b801292a1f9bf090f522e502661
bjick84 <bjick@web.de>
2023-02-15 11:29:37 +0000

    Sortiermöglichkeiten

---
+++ 05_SQL_SELECT/05_4_que_filter.sql
@@ -0,0 +1,25 @@
+/* QUERIES Filtern mit WHERE */
+
+/*
+    Eingrenzen/Filtern WHERE & AND/OR etc.
+    Eingrenzen/Filtern WHERE & LIKE + Parameter
+    Eingrenzen/Filtern WHERE & RegEx
+    Eingrenzen/Filtern WHERE & IN / NOT IN
+    Eingrenzen/Filtern WHERE & BETWEEN / NOT BETWEEN
+*/
+
+-- Eingrenzen/Filtern WHERE & AND/OR etc.
+-- Scharfe Suche
+
+SELECT
+	ticker AS "SYM",
+    c_name AS "Unternehmen",
+	price AS "Kurs ($)",
+    payouts AS "Zahlung p.a.",
+    dividend AS "Dividende",
+	CONCAT(sector, " | ", industry) AS "Operations"
+FROM stocks.ccc
+
+-- Begrenzung
+LIMIT 20
+;
