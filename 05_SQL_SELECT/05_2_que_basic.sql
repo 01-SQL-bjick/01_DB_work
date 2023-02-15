7be65a0bc6fe8df31fb1c73f92741c137b4204da
bjick84 <bjick@web.de>
2023-02-15 10:17:54 +0000

    SELECT LIMIT von bis++

--- 05_SQL_SELECT/05_2_que_basic.sql
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -5,3 +5,22 @@ Felder abfragen
 Felder kombinieren
 Ausgabe beschränken
 */
+
+-- Abfrage aller Spalten der Tab
+#SELECT * FROM stocks.ccc;
+
+-- Begrenzung: LIMIT
+-- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
+-- Kombinationen aus mehreren Feldern in neues Feld 
+-- hier mit Verknüpfung CONCAT() // String-Fkt.
+SELECT
+    id,
+    ticker AS "SYM",
+    price AS "Kurs in $",
+    c_name AS Unternehmen,
+    #industry Branche,
+    CONCAT(sector, " | ", industry) AS "Operation"
+FROM stocks.ccc
+LIMIT 200,10
+#LIMIT 5
+;
